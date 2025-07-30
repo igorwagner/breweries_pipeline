97d7786d78a86c332a4dc417d186f27b39cd2185
Igor <igorwagner.sdo@gmail.com>
2025-07-29 23:26:04 -0300

    style: update logger file with pre-commit hook

--- utils/logger.py
+++ utils/logger.py
@@ -7,9 +7,7 @@ logger = logging.getLogger("breweries_datalake_logger")
 if not logger.hasHandlers():
     logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler()
-    formatter = logging.Formatter(
-        "%(asctime)s — %(levelname)s — %(name)s — %(message)s"
-    )
+    formatter = logging.Formatter("%(asctime)s — %(levelname)s — %(name)s — %(message)s")
     handler.setFormatter(formatter)
     handler.setLevel(logging.DEBUG)
     logger.addHandler(handler)
